ee3596e26816ebb9faf7424f4ba7021ec4aa06b3
Nikoleta Tekemska <nikoletatt94@gmail.com>
2024-07-31 11:13:34 +0300

    Create ClientRepository.java

---
+++ server/src/main/java/com/bankigapp/server/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.bankigapp.server.repository;
+
+import com.bankigapp.server.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
